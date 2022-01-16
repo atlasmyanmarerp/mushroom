39020949fb7663016533d9533ce2e7a207c67a1f
Gam-safecoms <gam.hpang@safecoms.com>
2022-01-16 15:36:10 +0630

    update pos_customize

---
+++ pos_customized/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Customized Point of Sale',
+    'version': '1.0.1',
+    'category': 'Point of sale',
+    'sequence': 40,
+    'summary': 'Customized Point of Sale',
+    'author': 'Mohit Ghodasara',
+    'support': 'Mohit.Ghodasara@Yahoo.com',
+    'description': 'Customized Point of Sale',
+    'depends': ['point_of_sale','mushroom_customize'],
+    'data': [
+        'views/point_of_sale_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'qweb': [
+        'static/src/xml/OrderReceipt.xml',
+        'static/src/xml/DeliveryDatePopup.xml',
+    ],
+}
